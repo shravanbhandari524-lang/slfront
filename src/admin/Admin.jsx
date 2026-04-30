39a27f12acfebe7eb9d5bdd3071ac78340f9362b
shravanbhandari524-lang <shravanbhandari524@gmail.com>
2026-04-30 18:32:05 +0530

    added logout to the admin page

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -1,3 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { Rcont } from "../context/Rcontext.jsx";
 export default function Admin() {
-  return <div>admin</div>;
+  const { setToken } = useContext(Rcont);
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    await fetch("http://localhost:8080/auth/logout", {
+      method: "POST",
+      credentials: "include", // ← needed to send the cookie so backend can clear it
+    });
+    setToken("");
+    navigate("/login");
+  };
+  return (
+    <div>
+      admin<button onClick={handleLogout}>logout</button>
+    </div>
+  );
 }
